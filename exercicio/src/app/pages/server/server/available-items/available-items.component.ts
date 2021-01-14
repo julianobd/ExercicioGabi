14aeba2483093c2a1910c5fc6bc0182dd23e8780
GabrielaGaspar <72529661+GabiGaspar@users.noreply.github.com>
2021-01-14 11:03:51 -0300

    Adicionar e Deletar AvbItem
    Função Adicionar e Deletar AvbItem

--- exercicio/src/app/pages/server/server/available-items/available-items.component.ts
+++ exercicio/src/app/pages/server/server/available-items/available-items.component.ts
@@ -3,6 +3,7 @@ import { FormGroup,FormBuilder } from '@angular/forms';
 import { ServerListService } from 'src/app/shared/services/server-list.service';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-available-items',
@@ -10,6 +11,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./available-items.component.sass']
 })
 export class AvailableItemsComponent implements OnInit {
+  dataAvbItem:FormGroup;
   avbItem:any;
   dataServer:any;
   availableItems:any;
@@ -21,7 +23,21 @@ export class AvailableItemsComponent implements OnInit {
     private serverListService: ServerListService,
     private router:Router,
     private route:ActivatedRoute
-  ) { }
+  ) {
+    this.dataAvbItem = this.fb.group({
+      "name": '',
+        "health": '',
+        "vest": '',
+        "experience": '',
+        "hungry": '',
+        "money": '',
+        "statusPoint": '',
+        "isInitial": '',
+        "quantityInitial":'',
+        "isVirtual": '',
+    })
+
+   }
 
   ngOnInit(): void {
     this.serverListService. getdataServer().subscribe((res:any)=>
@@ -41,5 +57,53 @@ export class AvailableItemsComponent implements OnInit {
       console.log(this.avbItemID);
       this.router.navigate(['editAvb', this.avbItemID], {relativeTo: this.route})
   }
+  addAvb(){
+      this.avbItem.push({
+        name: this.dataAvbItem.value.name,
+        health: this.dataAvbItem.value.health,
+        vest: this.dataAvbItem.value.vest,
+        experience: this.dataAvbItem.value.experience,
+        hungry: this.dataAvbItem.value.hungry,
+        money: this.dataAvbItem.value.money,
+        statusPoint: this.dataAvbItem.value.statusPoint,
+        isInitial: this.dataAvbItem.value.isInitial,
+        quantityInitial:this.dataAvbItem.value.quantityInitial,
+        isVirtual: this.dataAvbItem.value.isVirtual,
+
+      })
+  }
+  saveAvb(){
+    const id = "00000000-0000-0000-0000-000000000000";
+    this.avbItem.forEach(element => {
+      element.id = id;
+    });
+    this.serverListService.updateAvb(this.avbItem).pipe(take(1)).subscribe(res=>{
+      console.log('Avb Item adicionado com sucesso', res)
+    })
+    this.dataAvbItem.reset({
+      name: '',
+        health: '',
+        vest: '',
+        experience: '',
+        hungry: '',
+        money: '',
+        statusPoint: '',
+        isInitial: '',
+        quantityInitial:'',
+        isVirtual: '',
+    })
+  }
+  delete(i){
+    this.avbItem.splice(i,1);
+    const id = "00000000-0000-0000-0000-000000000000";
+    this.avbItem.forEach((x) => {
+      x.id = id;
+    });
+    this.serverListService.updateAvb(this.avbItem).pipe(take(1)).subscribe((res:any)=>{
+      console.log('Avb Excluído',res)
+    })
+
+  }
+
 
 }
